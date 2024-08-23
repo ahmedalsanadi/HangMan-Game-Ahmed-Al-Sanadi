bfe2cfb381d8867fcbcdad25f93a8be9982c7e4a
ahmedalsanadi <ahmed11alsanadi11@gmail.com>
2024-08-24 02:16:18 +0300

    Implement initial Js setup for Hangman game
    - Set-up event listener for DOMContentLoaded to initialize the app.
      - Created the ControlleringWholeApp function to controll all functions in the game as afather.
      - Generate dynamic js letter buttons via the function createAlphabetKeys().
      - Fetch a random word from an external API via fetchRandomWord().
      - Create empty blanks based on the length of the random word using createBlanks() function.
      - Develop createBlanks function to display underscores representing letters in the random word.

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Example of what we want to accomplish [here](https://codepen.io/cathydutton/pen/
     4. Add a counter from 10 that will decrease by 1 with every wrong guess.
     4. Add a button to play again.
 
+
 2. JS:
     1. At the beginning use fetch API to retrieve a random word from this api https://random-word-api.herokuapp.com/word?number=1. Make the blanks equal the number of letters in the random word.
     2. Everytime the user clicks on one of the letters the following should happen:
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,56 @@
-s
+
+    const answerField = document.querySelector(".answer-field "); // where blanks and  correct letters are place
+    const alphabetButtonConatainer = document.querySelector(".alphabet-buttons-container");// contains buttons of letters
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+   
+    document.addEventListener("DOMContentLoaded" , () => {
+        ControlleringWholeApp();
+    });
+
+ /* --- this Function will controll the whole app (Father for all functions) -----*/   
+    async function ControlleringWholeApp()
+    {
+        createAlphabetKeys(letters);                  // print letters buttons
+        const randomWord = await fetchRandomWord();  //get the random word via API
+        createBlanks(randomWord);                   //creating blanks according to the lemgth of the random word.
+        
+
+    }
+
+async function fetchRandomWord() {
+
+    const response = await fetch("https://random-word-api.herokuapp.com/word?number=1");
+    const data = await response.json();
+    const randomWord = data[0];
+    return randomWord;
+}
+
+
+
+ function createBlanks(word) // recieve random word and print blanks according to its length
+ {
+    const randomWordLength= word.length;
+    let blanks =[];
+    for(let i = 0; i<= randomWordLength; i++)
+    {
+        blanks.push('_');
+    }
+    console.log(blanks.join(" ")); // join array elements in one string
+    answerField.innerHTML = blanks.join(" "); // to add Empty blanks 
+ }
+
+
+
+function createAlphabetKeys(lettersArray)
+{
+    lettersArray.forEach(letter => {
+       const letterBtn = document.createElement('button');
+       letterBtn.classList.add('letter-btn');
+       letterBtn.id = letter;
+       letterBtn.innerHTML =letter;
+       alphabetButtonConatainer.appendChild(letterBtn); 
+    });
+
+}
